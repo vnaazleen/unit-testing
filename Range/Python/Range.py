b2816f4dc39a4a47c8fe98354e201a1e8f946353
Vaseem Naazleen Shaik <vnaazleen@users.noreply.github.com>
2021-01-13 19:48:01 +0530

    Added minimal python code in range class

---
+++ Range/Python/Range.py
@@ -0,0 +1,5 @@
+class Range:
+    def __init__(self, start, limit):
+        # range class
+        self.start = start
+        self.limit = limit
---
+++ Range/Python/range_test.py
@@ -0,0 +1,6 @@
+import unittest
+import Range 
+
+# every test class should inherit from unittest.TestCase
+class RangeTest(unittest.TestCase):
+    def test
